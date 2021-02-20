b2c91585ddf67d22988395ace93dc0e4d6b6590b
Peter Klausing <s1856172@mail.zih.tu-dresden.de>
2021-02-20 23:18:08 +0100

    update definitions with more initialization
    * add toString

--- include/PivotControlMessages.h
+++ include/PivotControlMessages.h
@@ -1,21 +1,37 @@
 #ifndef PIVOT_CONTROL_MESSAGES_H
 #define PIVOT_CONTROL_MESSAGES_H
 
+#include <sstream>
+
 namespace pivot_control_messages
 {
-    typedef struct {
-        float yaw = 0;
+    struct DOFPose
+    {
         float pitch = 0;
+        float yaw = 0;
         float roll = 0;
         float transZ = 0;
-    } DOFPose;
-
-    typedef struct {
-        float yawMax, yawMin;
-        float pitchMax, pitchMin;
-        float rollMax, rollMin;
-        float transZMax, transZMin;
-    } DOFBoundaries;
+        std::string toString()
+        {
+            std::stringstream ss;
+            ss << "pitch:" << pitch
+               << " yaw:" << yaw
+               << " roll" << roll
+               << " transZ" << transZ;
+            return ss.str();
+        }
+    };
+    struct DOFBoundaries
+    {
+        float pitchMax = 0;
+        float pitchMin = 0;
+        float yawMax = 0;
+        float yawMin = 0;
+        float rollMax = 0;
+        float rollMin = 0;
+        float transZMax = 0;
+        float transZMin = 0;
+    };
 }
 
 #endif //PIVOT_CONTROL_MESSAGES_H
