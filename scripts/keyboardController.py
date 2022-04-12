06dd75ac68516a8e97b2294e85055c8066d0ae2f
Peter Klausing <peter.klausing@nct-dresden.de>
2022-04-12 18:44:40 +0200

    use Timer and threading to make it work again for publishing
    - split pose into cur_pose and target_pose

--- scripts/keyboardController.py
+++ scripts/keyboardController.py
@@ -7,6 +7,7 @@ from pygame.locals import K_ESCAPE, K_w, K_s, K_a, K_d, K_q, K_e, K_y, K_x, K_0
 import argparse
 from pivot_control_messages_ros.msg import LaparoscopeDOFPose
 from pivot_control_messages_ros.msg import LaparoscopeDOFBoundaries
+import threading
 
 
 def limitToOne(a):
@@ -29,20 +30,27 @@ class KeyboardDOFController:
             "target/laparoscope_dof_pose",
             LaparoscopeDOFPose, queue_size=10)
 
+        current_dof_pose_topic_name = "current/laparoscope_dof_pose"
+
+        self.target_pose = rospy.wait_for_message(current_dof_pose_topic_name, LaparoscopeDOFPose)
+
         # A subscriber to the topic '/turtle1/pose'. self.update_pose is called
         # when a message of type Pose is received.
         self.boundaries_subscriber = rospy.Subscriber(
             "laparoscope_dof_boundaries",
             LaparoscopeDOFBoundaries, self.update_boundaries)
         self.pose_subscriber = rospy.Subscriber(
-            "current/laparoscope_dof_pose",
+            current_dof_pose_topic_name,
             LaparoscopeDOFPose, self.update_pose)
 
         self.seq = 0
         self.pose = LaparoscopeDOFPose()
         self.boundaries = LaparoscopeDOFBoundaries()
-        # rospy.Timer(rospy.Duration(0.1), self.checkKeyboard)
+        self.mutex = threading.Lock()
         self.rate = rospy.Rate(10)
+        self.move = False
+
+        self.publish_timer = rospy.Timer(self.rate.sleep_dur, self.check_move)
 
         pygame.init()
         self.screen = pygame.display.set_mode((640, 480))
@@ -68,21 +76,34 @@ class KeyboardDOFController:
         self.boundaries = boundaries
 
     def update_pose(self, pose):
-        self.pose = pose
+        self.cur_pose = pose
 
-    def move(self):
+    def check_move(self, event):
+        self.mutex.acquire()
+        if self.move:
+            self.publish_move(event)
+        #     move_old = True
+        # if move_old and not self.move:
+        #     self.direction = "stop motion"
+        #     self.publish_move()
+        #     move_old = False
+        self.move = False
+        self.mutex.release()
+
+    def publish_move(self, event):
         pose = LaparoscopeDOFPose()
         self.seq = self.seq + 1
         pose.header.seq = self.seq
-        pose.header.stamp = rospy.Time()
+        pose.header.stamp = event.last_real
         pose.header.frame_id = "laparoscope"
-        pose.yaw = self.pose.yaw
-        pose.pitch = self.pose.pitch
-        pose.roll = self.pose.roll
-        pose.trans_z = self.pose.trans_z
+        pose.yaw = self.target_pose.yaw
+        pose.pitch = self.target_pose.pitch
+        pose.roll = self.target_pose.roll
+        pose.trans_z = self.target_pose.trans_z
         if self.direction == "stop motion":
             # just publish the last pose we got
-            self.pose_publisher.publish(self.pose)
+            #TODO: should we publish target or curent pose here?
+            self.pose_publisher.publish(self.target_pose)
             return
         # print("move further")
         # calculate the pitch allready caused by trans_z
@@ -137,29 +158,36 @@ class KeyboardDOFController:
         pose.trans_z =  max(pose.trans_z, self.boundaries.trans_z_min)
         pose.trans_z =  min(pose.trans_z, self.boundaries.trans_z_max)
 
-        if (self.boundaries.yaw_min <= pose.yaw <= self.boundaries.yaw_max and
+        if (not(self.boundaries.yaw_min <= pose.yaw <= self.boundaries.yaw_max and
                 self.boundaries.pitch_min <= pose.pitch <= self.boundaries.pitch_max and
                 self.boundaries.roll_min <= pose.roll <= self.boundaries.roll_max and
-                self.boundaries.trans_z_min <= pose.trans_z <= self.boundaries.trans_z_max):
-            if self.pose.trans_z != pose.trans_z and self.add_trans_pitch:
-                trans_pitch_new = math.asin(limitToOne(
-                    (math.sin(math.pi - self.camera_tilt) * pose.trans_z) /
-                    self.focus_distance
-                ))
-                pose.pitch = pose.pitch - (trans_pitch_new - trans_pitch)
-            if self.add_roll_correction:
-                tilt_pitch = pose.pitch - (
-                    self.camera_tilt if self.add_camera_tilt else 0)
-                vec1 = np.array([0, math.cos(tilt_pitch), math.sin(tilt_pitch)])
-                vec2 = np.array([math.sin(pose.yaw) * math.sin(tilt_pitch),
-                                 math.cos(tilt_pitch),
-                                 math.cos(pose.yaw) * math.sin(tilt_pitch)])
-                sign_pitch = 1 if pose.pitch > 0 else -1
-                sign_yaw = 1 if pose.yaw < 0 else -1
-                z_angle = np.dot(vec1, vec2)
-                if z_angle < 1.0:
-                    pose.roll = sign_yaw * sign_pitch * math.acos(z_angle)
-            self.pose_publisher.publish(pose)
+                self.boundaries.trans_z_min <= pose.trans_z <= self.boundaries.trans_z_max)):
+            rospy.logwarn("new target pose out of scope")
+            return
+
+        if self.pose.trans_z != pose.trans_z and self.add_trans_pitch:
+            trans_pitch_new = math.asin(limitToOne(
+                (math.sin(math.pi - self.camera_tilt) * pose.trans_z) /
+                self.focus_distance
+            ))
+            pose.pitch = pose.pitch - (trans_pitch_new - trans_pitch)
+        if self.add_roll_correction:
+            tilt_pitch = pose.pitch - (
+                self.camera_tilt if self.add_camera_tilt else 0)
+            vec1 = np.array([0, math.cos(tilt_pitch), math.sin(tilt_pitch)])
+            vec2 = np.array([math.sin(pose.yaw) * math.sin(tilt_pitch),
+                             math.cos(tilt_pitch),
+                             math.cos(pose.yaw) * math.sin(tilt_pitch)])
+            sign_pitch = 1 if pose.pitch > 0 else -1
+            sign_yaw = 1 if pose.yaw < 0 else -1
+            z_angle = np.dot(vec1, vec2)
+            if z_angle < 1.0:
+                pose.roll = sign_yaw * sign_pitch * math.acos(z_angle)
+        self.target_pose.pitch = pose.pitch
+        self.target_pose.yaw = pose.yaw
+        self.target_pose.roll = pose.roll
+        self.target_pose.trans_z = pose.trans_z
+        self.pose_publisher.publish(pose)
 
     def display(self):
         dirtext = self.font.render(self.direction, True, (255, 255, 255),
@@ -169,7 +197,7 @@ class KeyboardDOFController:
         dirtextRect.centery = self.screen.get_rect().centery
 
         str = "pitch:{:.2f} yaw:{:.2f} roll:{:.2f} trans_z:{:.2f}".format(
-            self.pose.pitch, self.pose.yaw, self.pose.roll, self.pose.trans_z)
+            self.cur_pose.pitch, self.cur_pose.yaw, self.cur_pose.roll, self.cur_pose.trans_z)
         posetext = self.font.render(str, True, (255, 255, 255), (159, 182, 205))
         posetextRect = posetext.get_rect()
         posetextRect.centerx = self.screen.get_rect().centerx
@@ -201,43 +229,45 @@ class KeyboardDOFController:
 
     def start(self):
         done = False
-        move_old = False
         while not done:
             self.display()
             pygame.event.pump()
             keys = pygame.key.get_pressed()
-            move = False
             if rospy.is_shutdown():
                 done = True
+                continue
             if keys[K_ESCAPE]:
                 done = True
+                break
+            self.mutex.acquire()
             if keys[K_0]:
                 self.direction = "0"
-                move = True
+                self.move = True
             if keys[K_w]:
                 self.direction = "up"
-                move = True
+                self.move = True
             if keys[K_s]:
                 self.direction = "down"
-                move = True
+                self.move = True
             if keys[K_a]:
                 self.direction = "left"
-                move = True
+                self.move = True
             if keys[K_d]:
                 self.direction = "right"
-                move = True
+                self.move = True
             if keys[K_q]:
                 self.direction = "out"
-                move = True
+                self.move = True
             if keys[K_e]:
                 self.direction = "in"
-                move = True
+                self.move = True
             if keys[K_y]:
                 self.direction = "rot_left"
-                move = True
+                self.move = True
             if keys[K_x]:
                 self.direction = "rot_right"
-                move = True
+                self.move = True
+            self.mutex.release()
             # if keys[K_]:
             #     self.add_camera_tilt = True
             #     self.direction = ""
@@ -246,18 +276,14 @@ class KeyboardDOFController:
             #     self.add_camera_tilt = False
             #     self.direction = ""
             #     move = True
-            if move:
-                self.move()
-                move_old = True
-            if move_old and not move:
-                self.direction = "stop motion"
-                self.move()
-                move_old = False
 
+    def stop(self):
+        self.publish_timer.shutdown()
 
 if __name__ == '__main__':
     try:
         x = KeyboardDOFController()
         x.start()
+        x.stop()
     except rospy.ROSInterruptException:
         pass
